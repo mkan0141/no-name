b8e0d176b5c55bf3e8a6581a1dd69d77b05f8ada
mkan0141 <kanzume0141@gmail.com>
2018-03-07 13:31:28 +0900

    fix: 一部モジュールでエラーが出ていたので修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,48 @@
 const path = require('path');
 const webpack = require('webpack');
+const publidDir = path.join(__dirname, '/public');
 
 module.exports = {
     entry: './app/src/index.jsx',
-
+    target: 'node',
     // バンドルファイルの出力場所
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, './public')
     },
     module: {
-        loaders: [{
-            exclude: '/node_modules/',
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'es2015'],
-            },
-        }],
-    },
+    rules: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+            presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+            presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.json'],
     },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: publidDir,
+    }
 };
