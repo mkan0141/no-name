d647e51a7f9813dc13f9d5493de0bc6d6e2f11e0
mkan0141 <kanzume0141@gmail.com>
2018-03-08 23:08:20 +0900

    add: App.jsxの追加

---
+++ app/src/js/component/App.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+var config =  require('./../config/config.js')
+import Twitter from 'twitter'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = ({
+      date: new Date(),
+      tweets: null
+    });
+    this.twitter = new Twitter({
+      consumer_key: config.consumer_key,
+      consumer_secret: config.consumer_secret,
+      access_token_key: config.access_token_key,
+      access_token_secret: config.access_token_secret
+    })
+    this.GetTweet = this.GetTweet.bind(this)
+    this.renderTimeline = this.renderTimeline.bind(this)
+  }
+
+  renderTimeline() {
+    if(this.state.tweets == null) {
+      console.log('tweet nothing')
+      return ;
+    }
+     const TweetsList = this.state.tweets.map((tweet) =>{
+       console.log(tweet.text)
+        return (
+          <li key={tweet.id}>{tweet.text}</li>
+        )
+      }
+    )
+    console.log(TweetsList)
+    return (
+      <ul>
+        {TweetsList}
+      </ul>
+    )
+  }
+
+  GetTweet() {
+    this.twitter.get('statuses/user_timeline', {count: 200}, (error, tweet, response) => {
+      if (!error) {
+        this.setState({
+          tweets: tweet
+        },() => console.log('in'))
+      }else{
+        console.log('error: ')
+        console.log(error)
+      }
+    });
+  }
+
+  render() {
+    this.GetTweet()
+    return (
+      <div>
+        <ul>{this.renderTimeline()}</ul>
+      </div>
+    )
+  }
+}
+
+export default App
