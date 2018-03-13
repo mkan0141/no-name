45bb3f4409610b609ae85ca8cb40758c8fc45fab
mkan0141 <kanzume0141@gmail.com>
2018-03-14 00:43:17 +0900

    update: timelineを自動更新できるようにした

--- app/src/js/component/App.jsx
+++ app/src/js/component/App.jsx
@@ -1,38 +1,44 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import Tweets from './Tweets'
 
-var config =  require('./../config/config.js')
+var config = require('./../config/config.js')
 import Twitter from 'twitter'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = ({
-      date: new Date(),
-      tweets: null
-    });
-    this.twitter = new Twitter({
-      consumer_key: config.consumer_key,
-      consumer_secret: config.consumer_secret,
-      access_token_key: config.access_token_key,
-      access_token_secret: config.access_token_secret
-    })
+    this.state = ({date: new Date(), tweets: null});
+    this.twitter = new Twitter({consumer_key: config.consumer_key, consumer_secret: config.consumer_secret, access_token_key: config.access_token_key, access_token_secret: config.access_token_secret})
     this.GetTweet = this.GetTweet.bind(this)
     this.renderTimeline = this.renderTimeline.bind(this)
+
+    this.twitter.get('statuses/home_timeline', {
+      count: 200
+    }, (error, tweet, response) => {
+      if (!error) {
+        this.setState({
+          tweets: tweet
+        }, () => console.log('in'))
+      } else {
+        console.log('error: ')
+        console.log(error)
+      }
+    });
   }
 
   renderTimeline() {
-    if(this.state.tweets == null) {
+    if (this.state.tweets == null) {
       console.log('tweet nothing')
-      return ;
+      return;
     }
-     const TweetsList = this.state.tweets.map((tweet) =>{
-       console.log(tweet.text)
-        return (
-          <li key={tweet.id} id="tweet">{tweet.text}</li>
-        )
-      }
-    )
-    console.log(TweetsList)
+
+    const TweetsList = this.state.tweets.map((tweet) => {
+      console.log(tweet)
+      return (
+        <li key={tweet.id} id="tweet">{tweet.text}</li>
+      )
+    })
+
     return (
       <ul>
         {TweetsList}
@@ -42,16 +48,23 @@ class App extends Component {
 
   GetTweet() {
     var stream = this.twitter.stream('user');
+    stream.on('data', (event) => {
+      //console.log(event.user.name + ': ' + event.text);
+      console.log('in GetTweet')
+      var tweetsArray = this.state.tweets.slice()
+      tweetsArray.push(event)
+      this.setState({
+        tweets: tweetsArray
+      }, () => {console.log('in state')})
 
-    stream.on('data', function(event) {
-  console.log(event.user.name + ': ' + event.text);
-});
+    });
 
-stream.on('error', function(error) {
-  throw error;
-});
-/*
-    this.twitter.stream('statuses/home_timeline', {count: 200}, (error, tweet, response) => {
+    stream.on('error', function(error) {
+      throw error;
+    });
+    /*
+
+    this.twitter.get('statuses/home_timeline', {count: 200}, (error, tweet, response) => {
       if (!error) {
         this.setState({
           tweets: tweet
@@ -64,11 +77,15 @@ stream.on('error', function(error) {
 */
   }
 
+  componentDidMount() {
+    console.log('in')
+    this.GetTweet();
+  }
+
   render() {
-    this.GetTweet()
     return (
       <div>
-        <ul>{this.renderTimeline()}</ul>
+        <Tweets tweets={this.state.tweets} />
       </div>
     )
   }
